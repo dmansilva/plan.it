f0aef4273d664b39c0f7e2da88cdb05a4244ba6c
dmansilva <djsilva@usfca.edu>
2017-03-25 21:30:54 -0700

    Initial Commit

---
+++ plan.it/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  plan.it
+//
+//  Created by Daniel Silva on 3/25/17.
+//  Copyright © 2017 D Silvv Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
